722124bf391197591fce58b6fe85e39c1aaef114
leandrosouzasnts <1eandrosouzasnts@gmail.com>
2023-11-19 12:45:07 -0300

    Add toCreatedClientEventTest

--- src/test/java/com/github/leandrosouzasnts/ClientTest.java
+++ src/test/java/com/github/leandrosouzasnts/ClientTest.java
@@ -1,10 +1,15 @@
 package com.github.leandrosouzasnts;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import org.junit.Test;
 
 import com.github.leandrosouzasnts.domain.Client;
+import com.github.leandrosouzasnts.event.EventDispatcher;
+import com.github.leandrosouzasnts.event.client.NotifyEmailClientCreatedHandler;
 
 public class ClientTest {
 
@@ -15,4 +20,16 @@ public class ClientTest {
         assertEquals(1, client.getClientId());
     }
     
+    @Test 
+    public void toCreatedClientEventTest(){
+        
+        Client client = mock(Client.class);
+        
+        EventDispatcher eventDispatcher = new EventDispatcher();
+        eventDispatcher.register(new NotifyEmailClientCreatedHandler());
+
+        client.toCreatedClient(eventDispatcher);
+
+        verify(client, times(1)).toCreatedClient(eventDispatcher);
+    }
 }
